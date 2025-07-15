35a28bade8d2e24a7bfbeb828bc1bc5250e7456b
Swastik Pratik <swastiksingh368@gmail.com>
2025-07-15 23:38:53 +0530

    cookies options

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,6 +6,8 @@ export const sendToken = (user, statusCode, message, res) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    sameSite: "none",
+    secure: true,
   };
 
   res.status(statusCode).cookie("token", token, cookieOptions).json({
